701b17c19ae449a3f7fed6f52cefcccd64026c8d
Panchdev Singh Chauhan <erpanchdev@gmail.com>
2024-03-14 09:22:13 +0530

    adjusted youtube script

--- pages/youtube/auth/watch_playlist_view.py
+++ pages/youtube/auth/watch_playlist_view.py
@@ -8,14 +8,14 @@ class PageWatchPlaylist(Page):
 
     # Page elements
     btn_loop_playlist = (By.CSS_SELECTOR, "ytd-page-manager button[aria-label='Loop playlist']")
-    btn_shuffle_playlist = (By.CSS_SELECTOR, "ytd-page-manager button[aria-label='Shuffle playlist']")
+    btn_shuffle_playlist = (By.CSS_SELECTOR, "ytd-page-manager button[aria-label='Shuffle playlist'][aria-pressed='false']")
 
     btn_i_like_this = (By.XPATH,
                        "//div[@id='actions-inner']//button[@title='I like this'][1]")
 
     # player controls
     div_player = (By.CSS_SELECTOR, "div[id='player']")
-    btn_volume_control = (By.CSS_SELECTOR, "span.ytp-volume-area")
+    btn_volume_control = (By.CSS_SELECTOR, "span.ytp-volume-area button[data-title-no-tooltip='Mute']")
 
     btn_settings_control = (By.CSS_SELECTOR, "button.ytp-settings-button")
     opt_playback_speed = (By.XPATH, "//div[contains(text(),'Playback speed')]")
@@ -25,14 +25,16 @@ class PageWatchPlaylist(Page):
     def click_loop_playlist_button(self):
         """Click on Loop Playlist button"""
 
-        self.click(*self.btn_loop_playlist)
-        self.wait_for_a_while(3)
+        if self.is_displayed(*self.btn_loop_playlist):
+            self.click(*self.btn_loop_playlist)
+            self.wait_for_a_while(3)
 
     def click_shuffle_playlist_button(self):
         """Click on Shuffle Playlist button"""
 
-        self.click(*self.btn_shuffle_playlist)
-        self.wait_for_a_while(3)
+        if self.is_displayed(*self.btn_shuffle_playlist):
+            self.click(*self.btn_shuffle_playlist)
+            self.wait_for_a_while(3)
 
     def click_like_video(self):
         """Like video if it is not yet liked by the current user"""
@@ -54,11 +56,35 @@ class PageWatchPlaylist(Page):
         """Click volume control"""
         player = self.find_element(*self.div_player)
         btn_share = self.find_element(By.CSS_SELECTOR, "div[id='actions'] button[aria-label='Share']")
+
         self.action_chain() \
             .move_to_element(player) \
-            .pause(2).click(self.find_element(*self.btn_volume_control)) \
-            .pause(2).click(self.find_element(*self.btn_settings_control)) \
+            .pause(2).click(self.find_element(*self.btn_volume_control))\
             .perform()
+
+        if self.is_displayed(*self.btn_volume_control):
+            try:
+                self.action_chain() \
+                    .move_to_element(player) \
+                    .pause(2).click(self.find_element(*self.btn_volume_control)) \
+                    .pause(2).click(self.find_element(*self.btn_settings_control)) \
+                    .perform()
+            except Exception as e:
+                self.action_chain() \
+                    .pause(2).click(self.find_element(*self.btn_volume_control)) \
+                    .pause(2).click(self.find_element(*self.btn_settings_control)) \
+                    .perform()
+
+        else:
+            try:
+                self.action_chain() \
+                    .move_to_element(player) \
+                    .pause(2).click(self.find_element(*self.btn_settings_control)) \
+                    .perform()
+            except Exception as e:
+                self.action_chain() \
+                    .pause(2).click(self.find_element(*self.btn_settings_control)) \
+                    .perform()
 
         self.action_chain() \
             .pause(2).click(self.find_element(*self.opt_playback_speed)) \
